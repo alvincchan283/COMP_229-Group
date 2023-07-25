80763add43732fbbbca250f1c7e1f64ce4c3765f
Ian Sze <iansze1124@gmail.com>
2023-07-25 16:22:07 -0400

    new component recipe-detail

---
+++ src/app/recipe-list/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { RecipeService } from '../recipe-list.service';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css'],
+})
+export class RecipeDetailComponent {
+  id: string = '';
+  recipe: Recipe = {
+    _id: '',
+    name: '',
+    desc: '',
+    img: '',
+  };
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paraMap: ParamMap) => {
+      if (paraMap.has('id')) {
+        this.id = paraMap.get('id') as string;
+        this.recipeService.getRecipeById(this.id).then((recipe) => {
+          this.recipe = {
+            _id: recipe._id,
+            name: recipe.name,
+            desc: recipe.desc,
+            img: recipe.img,
+          };
+        });
+      }
+    });
+  }
+}
