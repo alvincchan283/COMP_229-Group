eeedf5429e196cb28cd826c145a69c3d6bc16f71
saboshi69 <shui2967@gmail.com>
2023-08-07 21:37:45 -0400

    update search page

--- src/app/recipe-list/search-result/search-result.component.ts
+++ src/app/recipe-list/search-result/search-result.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RecipeService } from '../recipe-list.service';
 import { Recipe } from '../recipe.model';
 import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResultComponent {
+  @ViewChild('f') searchForm: NgForm | undefined;
+  searchKeyword: string = '';
   recipes: Recipe[] = [];
   recipeSubscription: Subscription = new Subscription();
 
-  constructor (private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router) {}
 
   ngOnInit(): void {
+    this.searchKeyword = this.recipeService.getSearchKeyword();
     this.recipes = this.recipeService.getFetchedRecipes();
   }
 
   ngOnDestroy() {
     this.recipeSubscription.unsubscribe();
+  }
+
+  onSearch() {
+    const keyword = this.searchForm?.value.name;
+    if (!keyword) return;
+
+    this.recipeService.searchRecipeByName(keyword).then(() => {
+      this.router.navigate(['/search-result']);
+    });
   }
 }
